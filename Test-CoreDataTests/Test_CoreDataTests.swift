29a9289b0b0f7c0729571cff8a6818fe27e6b5ee
Hassan Abbasi <hassan.abbasi@therovecompany.com>
2020-08-14 15:19:39 +0500

    Initial Commit

---
+++ Test-CoreDataTests/Test_CoreDataTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_CoreDataTests.swift
+//  Test-CoreDataTests
+//
+//  Created by Hassan Abbasi on 14/08/2020.
+//  Copyright © 2020 Hassan Abbasi. All rights reserved.
+//
+
+import XCTest
+@testable import Test_CoreData
+
+class Test_CoreDataTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
